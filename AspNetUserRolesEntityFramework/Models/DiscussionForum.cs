649050616cc6aa9252f3802e21229520486a3a82
Leonard Tkatchenko <leonard.tkatchenko@gmail.com>
2020-10-05 04:49:30 +1100

    Model Fix

--- AspNetUserRolesEntityFramework/Models/DiscussionForum.cs
+++ AspNetUserRolesEntityFramework/Models/DiscussionForum.cs
@@ -7,23 +7,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AspNetUserRolesEntityFramework.Models 
 {
-    public int Id { get; set; }
-
-    [Display(Name = "Post Date")]
-    public DateTime PostDate { get; set; }
-
-    [Display(Name = "User Name")]
-    public string UserName { get; set; }
-
-    [Required]
-    [Display(Name = "Topic Title")]
-    public string TopicTitle { get; set; }
-
-    [Required]
-    [Display(Name = "Message Content")]
-    public string MessageContent { get; set; }
-
     public class DiscussionForum
     {
+        public int Id { get; set; }
+
+        [Display(Name = "Post Date")]
+        public DateTime PostDate { get; set; }
+
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Topic Title")]
+        public string TopicTitle { get; set; }
+
+        [Required]
+        [Display(Name = "Message Content")]
+        public string MessageContent { get; set; }
     }
 }
