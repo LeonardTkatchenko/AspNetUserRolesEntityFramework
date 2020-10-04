a9eb3d64e7372a73c9ec0684089cbda044923fc2
Leonard Tkatchenko <leonard.tkatchenko@gmail.com>
2020-10-05 05:01:08 +1100

    Controller and View

--- AspNetUserRolesEntityFramework/Data/ApplicationDbContext.cs
+++ AspNetUserRolesEntityFramework/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using AspNetUserRolesEntityFramework.Models;
 
 namespace AspNetUserRolesEntityFramework.Data
 {
@@ -12,5 +13,6 @@ namespace AspNetUserRolesEntityFramework.Data
             : base(options)
         {
         }
+        public DbSet<AspNetUserRolesEntityFramework.Models.DiscussionForum> DiscussionForum { get; set; }
     }
 }
